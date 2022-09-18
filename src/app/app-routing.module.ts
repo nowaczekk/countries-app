2e4d5884162c898a36569c89fb4813b0b2fffc05
nowaczekk <kamil.nowaczek93@gmail.com>
2022-09-18 18:10:57 +0200

    add basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { SingleItemComponent } from './components/single-item/single-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: ListComponent },
+    { path: ':id', component: SingleItemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { SingleItemComponent } from './components/single-item/single-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    SingleItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
